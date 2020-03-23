2daaef4a206b0cbde48bfb7bd213201a5779b637
Teri Fash <teri.fash@gmail.com>
2020-03-23 23:41:37 +0200

    add cursor component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import AppHome from './App-home';
+import Cursor from './plugins/cursor-dot';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Cursor/>
         <AppHome/>
-    </div >
+      </div >
     )
   }
 }
