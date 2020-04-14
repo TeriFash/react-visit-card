4dc4773b8acb803e09b184255fd80b2d7e27932a
Teri Fash <teri.fash@gmail.com>
2020-04-14 08:11:11 +0300

    add global var styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './App.css';
+// import './App.css';
+import './styles/main.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render( <App />, document.getElementById('root'));
