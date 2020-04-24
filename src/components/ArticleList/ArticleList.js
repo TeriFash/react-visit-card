8048b050c328cbd49edcb7f7cbddf3349499e719
TeriFash <teri.fash@gmail.com>
2020-04-24 07:31:49 +0300

    integration Firebase

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,30 +1,44 @@
-
 import React, { Fragment, useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import Firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig from "../../utilities/data/firebase";
 import postData from "../../store/articles/art-one";
 
 import ArticleListItem from "../ArticleListItem";
 
+let db = null;
 
 const ArticleList = () => {
   const [posts, setPosts] = useState([]);
-    let navigate = useNavigate();
-
+  let navigate = useNavigate();
 
   useEffect(() => {
     setPosts(postData);
   }, []);
 
+  useEffect(() => {
+    // Initialize Firebase
+    Firebase.initializeApp(firebaseConfig);
+    db = Firebase.firestore();
 
+    db.collection("articles").onSnapshot((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log('---', doc.data());
+        
+      });
+        
+    });
+  }, []);
 
   return (
-      
-    <div className="Article-list">
-    
+    <div className="Article-list"> 
       {posts.map((post, postIndex) => {
+        
         const clickedMore = (event) => {
-            event.preventDefault();
-            navigate(post.id, {replace: true})
+          event.preventDefault();
+          navigate(post.id, { replace: true });
         };
         return (
           <Fragment key={postIndex}>
@@ -34,12 +48,9 @@ const ArticleList = () => {
               content={post.content}
               isOnlySummary={true}
             />
-            <button onClick={clickedMore}>
-                More
-            </button>
+            <button onClick={clickedMore}>More</button>
 
             {/* <Link to={post.id}>More</Link>{" "} */}
-
           </Fragment>
         );
       })}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,28 @@ import React, { Component } from "react";
 import MainSocialLink from "../MainSocialLink";
 import MainLink from "../MainLink";
 import MainContainer from "../MainContainer";
+import load from "../../utilities/data/load";
 
 // import './Home.scss'
 
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      data: null,
+      term: "",
+      active: 0,
+    };
+  }
+
+  loadData() {
+    // Загружаем данные из файла, переданного в качестве параметра
+    load(this.props.data).then(users => {
+      // После загрузки обновляем состояние
+      this.setState({
+        data: JSON.parse(users)
+      });
+    });
   }
 
   render() {
@@ -19,6 +34,10 @@ class Home extends Component {
           <MainLink />
           <MainSocialLink />
         </MainContainer>
+
+
+        <div data="api.json"></div>
+
       </>
     );
   }
