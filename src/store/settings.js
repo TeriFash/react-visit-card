208c43eeb10dadfeb9e3c92b1c58530c1d037934
Teri Fash <teri.fash@gmail.com>
2020-07-09 11:38:21 +0300

    add setting

---
+++ src/store/settings.js
@@ -0,0 +1,14 @@
+// settings
+
+export default {
+    cvLinks: {
+      en: {
+        view: 'https://docs.google.com/document/d/1QUN8eLY4xA-7wuqlNIEfFJyETuEGKz9e4DqD8incN-8/edit?usp=sharing',
+        download: 'https://docs.google.com/document/d/1QUN8eLY4xA-7wuqlNIEfFJyETuEGKz9e4DqD8incN-8/export?format=pdf'
+      },
+      ru: {
+        view: 'https://docs.google.com/document/d/1-wco4u22lsKdYxYPDjzX-7awq_xub0Yla-ooK6EBfQI/edit?usp=sharing',
+        download: 'https://docs.google.com/document/d/1-wco4u22lsKdYxYPDjzX-7awq_xub0Yla-ooK6EBfQI/export?format=pdf'
+      },
+    },
+}
